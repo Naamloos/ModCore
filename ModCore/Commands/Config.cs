1afc999d4cdbc0548159e610cdc363d324894d96
chrishansen69 <cadrekucra@gmx.com>
2017-10-06 23:13:06 -0300

    add configuration for linkfilter

--- ModCore/Commands/Config.cs
+++ ModCore/Commands/Config.cs
@@ -48,10 +48,17 @@ namespace ModCore.Commands
             var muted = gcfg.MuteRoleId != 0 ? ctx.Guild.GetRole(gcfg.MuteRoleId) : null;
             embed.AddField("Muted Role", muted != null ? muted.Mention : "Not configured or missing", true);
 
-            var cfgib = gcfg.InviteBlocker;
-            embed.AddField("Invite Blocker", cfgib.Enable ? "Enabled" : "Disabled", true);
+            var cfgib = gcfg.Linkfilter;
+            embed.AddField("Linkfilter", cfgib.Enable ? "Enabled" : "Disabled", true);
             if (cfgib.Enable)
             {
+                embed.AddField("Linkfilter modules",
+                    $"{(cfgib.BlockInviteLinks ? "✔" : "✖")}anti-invites, " +
+                    $"{(cfgib.BlockBooters ? "✔" : "✖")}anti-ddos, " +
+                    $"{(cfgib.BlockIpLoggers ? "✔" : "✖")}anti-ip loggers, " +
+                    $"{(cfgib.BlockShockSites ? "✔" : "✖")}anti-shock sites, " +
+                    $"{(cfgib.BlockUrlShorteners ? "✔" : "✖")}anti-url shorteners", true);
+                
                 if (cfgib.ExemptRoleIds.Any())
                 {
                     var roles = cfgib.ExemptRoleIds
@@ -59,7 +66,7 @@ namespace ModCore.Commands
                         .Where(xr => xr != null)
                         .Select(xr => xr.Mention);
 
-                    embed.AddField("Invite Blocker-exempt roles", string.Join(", ", roles), true);
+                    embed.AddField("Linkfilter-exempt roles", string.Join(", ", roles), true);
                 }
 
                 if (cfgib.ExemptUserIds.Any())
@@ -67,14 +74,14 @@ namespace ModCore.Commands
                     var users = cfgib.ExemptUserIds
                         .Select(xid => $"<@!{xid}>");
 
-                    embed.AddField("Invite Blocker-exempt users", string.Join(", ", users), true);
+                    embed.AddField("Linkfilter-exempt users", string.Join(", ", users), true);
                 }
 
                 if (cfgib.ExemptInviteGuildIds.Any())
                 {
                     var guilds = string.Join(", ", cfgib.ExemptInviteGuildIds);
 
-                    embed.AddField("Invite Blocker-exempt invite targets", guilds, true);
+                    embed.AddField("Linkfilter-exempt invite targets", guilds, true);
                 }
             }
 
@@ -188,46 +195,138 @@ namespace ModCore.Commands
             await ctx.RespondAsync(role == null ? "Muting disabled." : $"Mute role set to {role.Mention}.");
         }
 
-        [Group("inviteblocker"), Aliases("invite", "ib"), Description("Invite Blocker configuration commands.")]
+        [Group("linkfilter"), Aliases("inviteblocker", "invite", "ib", "filter", "lf"), Description("Linkfilter configuration commands.")]
         public class InviteBlocker
         {
-            [Command("enable"), Aliases("on"), Description("Enables invite blocker for this guild.")]
+            [Command("enable"), Aliases("on"), Description("Enables linkfilter for this guild.")]
             public async Task EnableAsync(CommandContext ctx)
             {
                 var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
-                cfg.InviteBlocker.Enable = true;
+                cfg.Linkfilter.Enable = true;
                 await ctx.SetGuildSettingsAsync(cfg);
                 await ctx.Message.CreateReactionAsync(CheckMark);
             }
 
-            [Command("disable"), Aliases("off"), Description("Disables invite blocker for this guild.")]
+            [Command("disable"), Aliases("off"), Description("Disables linkfilter for this guild.")]
             public async Task DisableAsync(CommandContext ctx)
             {
                 var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
-                cfg.InviteBlocker.Enable = false;
+                cfg.Linkfilter.Enable = false;
                 await ctx.SetGuildSettingsAsync(cfg);
                 await ctx.Message.CreateReactionAsync(CheckMark);
+            }
+
+            [Group("modules"), Aliases("mod", "m", "s"), Description("Commands to toggle linkfilter modules for this guild.")]
+            public class Modules
+            {
+                [Group("all"), Aliases("a", "0"), Description("Commands to manage all linkfilter modules at once.")]
+                public class AllModules
+                {
+                    [Command("off"), Aliases("false", "f", "0"), Description("Disables all linkfilter modules for this guild.")]
+                    public async Task DisableAllLinkfilterModulesAsync(CommandContext ctx)
+                    {
+                        var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
+                        cfg.Linkfilter.BlockBooters = false;
+                        cfg.Linkfilter.BlockInviteLinks = false;
+                        cfg.Linkfilter.BlockIpLoggers = false;
+                        cfg.Linkfilter.BlockShockSites = false;
+                        cfg.Linkfilter.BlockUrlShorteners = false;
+                        await ctx.SetGuildSettingsAsync(cfg);
+                        await ctx.Message.CreateReactionAsync(CheckMark);
+                    }
+                    
+                    [Command("on"), Aliases("true", "t", "1"), Description("Enables all linkfilter modules for this guild.")]
+                    public async Task EnableAllLinkfilterModulesAsync(CommandContext ctx)
+                    {
+                        var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
+                        cfg.Linkfilter.BlockBooters = true;
+                        cfg.Linkfilter.BlockInviteLinks = true;
+                        cfg.Linkfilter.BlockIpLoggers = true;
+                        cfg.Linkfilter.BlockShockSites = true;
+                        cfg.Linkfilter.BlockUrlShorteners = true;
+                        await ctx.SetGuildSettingsAsync(cfg);
+                        await ctx.Message.CreateReactionAsync(CheckMark);
+                    }
+                    
+                    [Command("default"), Aliases("def", "d", "2"), Description("Sets all linkfilter modules to default for this guild.")]
+                    public async Task RestoreDefaultAllLinkfilterModulesAsync(CommandContext ctx)
+                    {
+                        var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
+                        cfg.Linkfilter.BlockBooters = true;
+                        cfg.Linkfilter.BlockInviteLinks = true;
+                        cfg.Linkfilter.BlockIpLoggers = true;
+                        cfg.Linkfilter.BlockShockSites = true;
+                        cfg.Linkfilter.BlockUrlShorteners = false;
+                        await ctx.SetGuildSettingsAsync(cfg);
+                        await ctx.Message.CreateReactionAsync(CheckMark);
+                    }
+                }
+
+                [Command("booters"), Aliases("booter", "boot", "ddos", "b", "1"), Description("Toggle blocking booter/DDoS sites for this guild.")]
+                public async Task ToggleBlockBootersAsync(CommandContext ctx)
+                {
+                    var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
+                    cfg.Linkfilter.BlockBooters = !cfg.Linkfilter.BlockBooters;
+                    await ctx.SetGuildSettingsAsync(cfg);
+                    await ctx.Message.RespondAsync($"{(cfg.Linkfilter.BlockBooters ? "enabled" : "disabled")} this module");
+                }
+
+                [Command("invites"), Aliases("invitelinks", "invite", "inv", "i", "2"), Description("Toggle blocking invite links for this guild.")]
+                public async Task ToggleBlockInviteLinksAsync(CommandContext ctx)
+                {
+                    var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
+                    cfg.Linkfilter.BlockInviteLinks = !cfg.Linkfilter.BlockInviteLinks;
+                    await ctx.SetGuildSettingsAsync(cfg);
+                    await ctx.Message.RespondAsync($"{(cfg.Linkfilter.BlockInviteLinks ? "enabled" : "disabled")} this module");
+                }
+
+                [Command("iploggers"), Aliases("iplogs", "ips", "ip", "3"), Description("Toggle blocking IP logger sites for this guild.")]
+                public async Task ToggleBlockIpLoggersAsync(CommandContext ctx)
+                {
+                    var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
+                    cfg.Linkfilter.BlockIpLoggers = !cfg.Linkfilter.BlockIpLoggers;
+                    await ctx.SetGuildSettingsAsync(cfg);
+                    await ctx.Message.RespondAsync($"{(cfg.Linkfilter.BlockIpLoggers ? "enabled" : "disabled")} this module");
+                }
+
+                [Command("shocksites"), Aliases("shock", "shocks", "gore", "g", "4"), Description("Toggle blocking shock/gore sites for this guild.")]
+                public async Task ToggleBlockShockSitesAsync(CommandContext ctx)
+                {
+                    var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
+                    cfg.Linkfilter.BlockShockSites = !cfg.Linkfilter.BlockShockSites;
+                    await ctx.SetGuildSettingsAsync(cfg);
+                    await ctx.Message.RespondAsync($"{(cfg.Linkfilter.BlockShockSites ? "enabled" : "disabled")} this module");
+                }
+
+                [Command("urlshortener"), Aliases("urlshorteners", "urlshort", "urls", "url", "u", "5"), Description("Toggle blocking URL shortener links for this guild.")]
+                public async Task ToggleBlockUrlShortenersAsync(CommandContext ctx)
+                {
+                    var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
+                    cfg.Linkfilter.BlockUrlShorteners = !cfg.Linkfilter.BlockUrlShorteners;
+                    await ctx.SetGuildSettingsAsync(cfg);
+                    await ctx.Message.RespondAsync($"{(cfg.Linkfilter.BlockUrlShorteners ? "enabled" : "disabled")} this module");
+                }
             }
 
             [Group("user"), Aliases("usr", "u"), Description("User exemption management commands.")]
             public class User
             {
-                [Command("exempt"), Aliases("x"), Description("Exempts user from invite checks.")]
-                public async Task ExemptAsync(CommandContext ctx, [RemainingText, Description("Member to exempt from invite checks.")] DiscordMember mbr)
+                [Command("exempt"), Aliases("x"), Description("Exempts user from linkfilter checks.")]
+                public async Task ExemptAsync(CommandContext ctx, [RemainingText, Description("Member to exempt from linkfilter checks.")] DiscordMember mbr)
                 {
                     var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
-                    var ibx = cfg.InviteBlocker.ExemptUserIds;
+                    var ibx = cfg.Linkfilter.ExemptUserIds;
                     if (!ibx.Contains(mbr.Id))
                         ibx.Add(mbr.Id);
                     await ctx.SetGuildSettingsAsync(cfg);
                     await ctx.Message.CreateReactionAsync(CheckMark);
                 }
 
-                [Command("unexempt"), Aliases("ux"), Description("Unexempts user from invite checks.")]
-                public async Task UnexemptAsync(CommandContext ctx, [RemainingText, Description("Member to unexempt from invite checks.")] DiscordMember mbr)
+                [Command("unexempt"), Aliases("ux"), Description("Unexempts user from linkfilter checks.")]
+                public async Task UnexemptAsync(CommandContext ctx, [RemainingText, Description("Member to unexempt from linkfilter checks.")] DiscordMember mbr)
                 {
                     var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
-                    var ibx = cfg.InviteBlocker.ExemptUserIds;
+                    var ibx = cfg.Linkfilter.ExemptUserIds;
                     if (ibx.Contains(mbr.Id))
                         ibx.Remove(mbr.Id);
                     await ctx.SetGuildSettingsAsync(cfg);
@@ -238,22 +337,22 @@ namespace ModCore.Commands
             [Group("role"), Aliases("r"), Description("Role exemption management commands.")]
             public class Role
             {
-                [Command("exempt"), Aliases("x"), Description("Exempts user from invite checks.")]
-                public async Task ExemptAsync(CommandContext ctx, [RemainingText, Description("Role to exempt from invite checks.")] DiscordRole rl)
+                [Command("exempt"), Aliases("x"), Description("Exempts role from linkfilter checks.")]
+                public async Task ExemptAsync(CommandContext ctx, [RemainingText, Description("Role to exempt from linkfilter checks.")] DiscordRole rl)
                 {
                     var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
-                    var ibx = cfg.InviteBlocker.ExemptRoleIds;
+                    var ibx = cfg.Linkfilter.ExemptRoleIds;
                     if (!ibx.Contains(rl.Id))
                         ibx.Add(rl.Id);
                     await ctx.SetGuildSettingsAsync(cfg);
                     await ctx.Message.CreateReactionAsync(CheckMark);
                 }
 
-                [Command("unexempt"), Aliases("ux"), Description("Unexempts user from invite checks.")]
-                public async Task UnexemptAsync(CommandContext ctx, [RemainingText, Description("Role to unexempt from invite checks.")] DiscordRole rl)
+                [Command("unexempt"), Aliases("ux"), Description("Unexempts role from linkfilter checks.")]
+                public async Task UnexemptAsync(CommandContext ctx, [RemainingText, Description("Role to unexempt from linkfilter checks.")] DiscordRole rl)
                 {
                     var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
-                    var ibx = cfg.InviteBlocker.ExemptRoleIds;
+                    var ibx = cfg.Linkfilter.ExemptRoleIds;
                     if (ibx.Contains(rl.Id))
                         ibx.Remove(rl.Id);
                     await ctx.SetGuildSettingsAsync(cfg);
@@ -261,10 +360,10 @@ namespace ModCore.Commands
                 }
             }
 
-            [Group("guild"), Aliases("i"), Description("Invite target exemption management commands,")]
+            [Group("guild"), Aliases("invite", "i"), Description("Invite target exemption management commands,")]
             public class Guild
             {
-                [Command("exempt"), Aliases("x"), Description("Exempts user from invite checks.")]
+                [Command("exempt"), Aliases("x"), Description("Exempts code from invite checks.")]
                 public async Task ExemptAsync(CommandContext ctx, [RemainingText, Description("Invite code to exempt from invite checks.")] string invite)
                 {
                     var inv = await ctx.Client.GetInviteByCodeAsync(invite);
@@ -272,14 +371,14 @@ namespace ModCore.Commands
                         return;
 
                     var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
-                    var ibx = cfg.InviteBlocker.ExemptInviteGuildIds;
+                    var ibx = cfg.Linkfilter.ExemptInviteGuildIds;
                     if (!ibx.Contains(inv.Guild.Id))
                         ibx.Add(inv.Guild.Id);
                     await ctx.SetGuildSettingsAsync(cfg);
                     await ctx.Message.CreateReactionAsync(CheckMark);
                 }
 
-                [Command("unexempt"), Aliases("ux"), Description("Unexempts user from invite checks.")]
+                [Command("unexempt"), Aliases("ux"), Description("Unexempts code from invite checks.")]
                 public async Task UnexemptAsync(CommandContext ctx, [RemainingText, Description("Invite code to unexempt from invite checks.")] string invite)
                 {
                     var inv = await ctx.Client.GetInviteByCodeAsync(invite);
@@ -287,7 +386,7 @@ namespace ModCore.Commands
                         return;
 
                     var cfg = ctx.GetGuildSettings() ?? new GuildSettings();
-                    var ibx = cfg.InviteBlocker.ExemptInviteGuildIds;
+                    var ibx = cfg.Linkfilter.ExemptInviteGuildIds;
                     if (ibx.Contains(inv.Guild.Id))
                         ibx.Remove(inv.Guild.Id);
                     await ctx.SetGuildSettingsAsync(cfg);
