cf5f234a9eb64b3532c19480b4421f9bbae13cd2
Mateusz Brawański <matiz100@windowslive.com>
2017-10-07 02:57:40 +0200

    Late-night-code role state implementation.

--- ModCore/Listeners/RoleState.cs
+++ ModCore/Listeners/RoleState.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using DSharpPlus;
 using DSharpPlus.EventArgs;
 using ModCore.Database;
 using ModCore.Logic;
@@ -56,6 +54,32 @@ namespace ModCore.Listeners
                 return;
             var rs = cfg.RoleState;
 
+            var gld = ea.Guild;
+            var roleids = db.RolestateRoles.SingleOrDefault(xs => xs.GuildId == (long)ea.Guild.Id && xs.MemberId == (long)ea.Member.Id);
+            var chperms = db.RolestateOverrides.Where(xs => xs.GuildId == (long)ea.Guild.Id && xs.MemberId == (long)ea.Member.Id);
+
+            if (roleids != null)
+            {
+                var roles = roleids.RoleIds
+                    .Except(rs.IgnoredRoleIds)
+                    .Select(xid => gld.GetRole((ulong)xid))
+                    .Where(xr => xr != null);
+
+                if (roles.Any())
+                    await ea.Member.ReplaceRolesAsync(roles, "Restoring role state.");
+            }
+
+            if (chperms.Any())
+            {
+                foreach (var chperm in chperms)
+                {
+                    var chn = gld.GetChannel((ulong)chperm.ChannelId);
+                    if (chn == null)
+                        continue;
+
+                    await chn.AddOverwriteAsync(ea.Member, (Permissions)chperm.PermsAllow, (Permissions)chperm.PermsDeny, "Restoring role state");
+                }
+            }
         }
 
         [AsyncListener(EventTypes.GuildMemberUpdated)]
@@ -67,39 +91,110 @@ namespace ModCore.Listeners
                 return;
             var rs = cfg.RoleState;
 
+            var gld = ea.Guild;
+            var roleids = db.RolestateRoles.SingleOrDefault(xs => xs.GuildId == (long)ea.Guild.Id && xs.MemberId == (long)ea.Member.Id);
+
+            if (roleids == null)
+            {
+                roleids = new DatabaseRolestateRoles
+                {
+                    GuildId = (long)ea.Guild.Id,
+                    MemberId = (long)ea.Member.Id
+                };
+            }
+
+            roleids.RoleIds = ea.RolesAfter
+                .Select(xr => xr.Id)
+                .Except(rs.IgnoredRoleIds)
+                .Select(xid => (long)xid)
+                .ToArray();
+            db.RolestateRoles.Update(roleids);
+            await db.SaveChangesAsync();
         }
 
         [AsyncListener(EventTypes.ChannelDeleted)]
         public static async Task OnChannelRemove(ModCoreShard shard, ChannelDeleteEventArgs ea)
         {
+            if (ea.Guild == null)
+                return;
+
             var db = shard.Database.CreateContext();
             var cfg = ea.Guild.GetGuildSettings(db);
             if (cfg == null || !cfg.RoleState.Enable)
                 return;
             var rs = cfg.RoleState;
 
+            var chperms = db.RolestateOverrides.Where(xs => xs.GuildId == (long)ea.Guild.Id && xs.ChannelId == (long)ea.Channel.Id);
+
+            if (chperms.Any())
+            {
+                db.RolestateOverrides.RemoveRange(chperms);
+                await db.SaveChangesAsync();
+            }
         }
 
-        [AsyncListener(EventTypes.ChannelCreated)]
-        public static async Task OnChannelCreate(ModCoreShard shard, ChannelCreateEventArgs ea)
-        {
-            var db = shard.Database.CreateContext();
-            var cfg = ea.Guild.GetGuildSettings(db);
-            if (cfg == null || !cfg.RoleState.Enable)
-                return;
-            var rs = cfg.RoleState;
+        // not necessary right now
+        //[AsyncListener(EventTypes.ChannelCreated)]
+        //public static async Task OnChannelCreate(ModCoreShard shard, ChannelCreateEventArgs ea)
+        //{
+        //    var db = shard.Database.CreateContext();
+        //    var cfg = ea.Guild.GetGuildSettings(db);
+        //    if (cfg == null || !cfg.RoleState.Enable)
+        //        return;
+        //    var rs = cfg.RoleState;
 
-        }
+        //}
 
         [AsyncListener(EventTypes.ChannelUpdated)]
         public static async Task OnChannelUpdate(ModCoreShard shard, ChannelUpdateEventArgs ea)
         {
+            if (ea.Guild == null)
+                return;
+
             var db = shard.Database.CreateContext();
             var cfg = ea.Guild.GetGuildSettings(db);
             if (cfg == null || !cfg.RoleState.Enable)
                 return;
             var rs = cfg.RoleState;
 
+            if (rs.IgnoredChannelIds.Contains(ea.ChannelAfter.Id))
+                return;
+
+            var os = ea.ChannelAfter.PermissionOverwrites.Where(xo => xo.Type == "member").ToDictionary(xo => (long)xo.Id, xo => xo);
+            var osids = os.Select(xo => xo.Key).ToArray();
+
+            var chperms = db.RolestateOverrides.Where(xs => xs.GuildId == (long)ea.Guild.Id && xs.ChannelId == (long)ea.ChannelAfter.Id)
+                .ToDictionary(xs => xs.MemberId, xs => xs);
+            var permids = chperms.Select(xo => xo.Key).ToArray();
+
+            var del = permids.Except(osids);
+            var add = osids.Except(permids);
+            var mod = osids.Intersect(permids);
+
+            if (del.Any())
+                db.RolestateOverrides.RemoveRange(del.Select(xid => chperms[xid]));
+
+            if (add.Any())
+                await db.RolestateOverrides.AddRangeAsync(add.Select(xid => new DatabaseRolestateOverride
+                {
+                    ChannelId = (long)ea.ChannelAfter.Id,
+                    GuildId = (long)ea.Guild.Id,
+                    MemberId = xid,
+                    PermsAllow = (long)os[xid].Allow,
+                    PermsDeny = (long)os[xid].Deny
+                }));
+
+            if (mod.Any())
+                foreach (var xid in mod)
+                {
+                    chperms[xid].PermsAllow = (long)os[xid].Allow;
+                    chperms[xid].PermsDeny = (long)os[xid].Deny;
+
+                    db.RolestateOverrides.Update(chperms[xid]);
+                }
+
+            if (del.Any() || add.Any() || mod.Any())
+                await db.SaveChangesAsync();
         }
     }
 }
