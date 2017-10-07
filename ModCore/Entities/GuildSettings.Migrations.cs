d62a70f765d7a0feaa304c732fd183700a6e78c7
chrishansen69 <cadrekucra@gmx.com>
2017-10-06 22:41:38 -0300

    implement linkfilter

---
+++ ModCore/Entities/GuildSettings.Migrations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ModCore.Entities
+{
+    public partial class GuildSettings
+    {
+        /// <summary>
+        /// Allows deserializing obsolete InviteBlocker setting, but not serialization.
+        /// </summary>
+        [JsonProperty("invite_blocker")]
+        [Obsolete("Use " + nameof(Linkfilter) + " instead!")]
+        public GuildLinkfilterSettings InviteBlocker
+        {
+            // no getter!
+            set => Linkfilter = value;
+        }
+    }
+}
