47b47d8ea5ce765df984ba35e4155bd7245a9245
NaamloosDT <ryandejonge@outlook.com>
2017-10-08 23:43:07 +0200

    Reimplemented SSG's Guild Extensions

--- ModCore/Extensions.cs
+++ ModCore/Extensions.cs
@@ -5,11 +5,140 @@ using DSharpPlus.Entities;
 using ModCore.Database;
 using ModCore.Entities;
 using System.Collections.Generic;
+using System;
 
 namespace ModCore
 {
     public static class Extensions
     {
+        /// <summary>
+        /// Executes an asynchronous function on the GuildSettings object, that can mutate it.
+        /// The resulting GuildSettings will be automatically persisted.
+        /// If the guild is not configured, a new GuildSettings object will be created.
+        /// </summary>
+        /// <param name="ctx">This object</param>
+        /// <param name="exec">The function to call</param>
+        /// <returns>Asynchronous task resolving to the CommandContext, if you wish to reuse it.</returns>
+        public static async Task<CommandContext> WithGuildSettings(this CommandContext ctx, Func<GuildSettings, Task> exec)
+        {
+            var cfg = GetGuildSettings(ctx) ?? new GuildSettings();
+            await exec(cfg);
+            await ctx.SetGuildSettingsAsync(cfg);
+            return ctx;
+        }
+
+        /// <summary>
+        /// Executes a synchronous function on the GuildSettings object, that can mutate it.
+        /// The resulting GuildSettings will be automatically persisted.
+        /// If the guild is not configured, a new GuildSettings object will be created.
+        /// </summary>
+        /// <param name="ctx">This object</param>
+        /// <param name="exec">The function to call</param>
+        /// <returns>Asynchronous task resolving to the CommandContext, if you wish to reuse it.</returns>
+        public static async Task<CommandContext> WithGuildSettings(this CommandContext ctx, Action<GuildSettings> exec)
+        {
+            var cfg = GetGuildSettings(ctx) ?? new GuildSettings();
+            exec(cfg);
+            await ctx.SetGuildSettingsAsync(cfg);
+            return ctx;
+        }
+
+        /// <summary>
+        /// Executes an asynchronous function <c>there</c> on the GuildSettings object if the guild is configured.
+        /// Executes <c>notThere</c> otherwise.
+        /// The resulting GuildSettings will be automatically persisted.
+        /// </summary>
+        /// <param name="ctx">This object</param>
+        /// <param name="there">The function to call if the guild is configured</param>
+        /// <param name="notThere">The function to call if the guild is not configured</param>
+        /// <returns>Asynchronous task resolving to the CommandContext, if you wish to reuse it.</returns>
+        public static async Task<CommandContext> IfGuildSettings(this CommandContext ctx, Func<GuildSettings, Task> there, Func<Task> notThere)
+        {
+            var cfg = GetGuildSettings(ctx);
+            if (cfg == null)
+            {
+                await notThere();
+            }
+            else
+            {
+                await there(cfg);
+                await ctx.SetGuildSettingsAsync(cfg);
+            }
+            return ctx;
+        }
+
+        /// <summary>
+        /// Executes a synchronous function <c>there</c> on the GuildSettings object if the guild is configured.
+        /// Executes <c>notThere</c> otherwise.
+        /// The resulting GuildSettings will be automatically persisted.
+        /// </summary>
+        /// <param name="ctx">This object</param>
+        /// <param name="there">The function to call if the guild is configured</param>
+        /// <param name="notThere">The function to call if the guild is not configured</param>
+        /// <returns>Asynchronous task resolving to the CommandContext, if you wish to reuse it.</returns>
+        public static async Task<CommandContext> IfGuildSettings(this CommandContext ctx, Action<GuildSettings> there, Action notThere)
+        {
+            var cfg = GetGuildSettings(ctx);
+            if (cfg == null)
+            {
+                notThere();
+            }
+            else
+            {
+                there(cfg);
+                await ctx.SetGuildSettingsAsync(cfg);
+            }
+            return ctx;
+        }
+
+        /// <summary>
+        /// Executes an asynchronous function <c>there</c> on the GuildSettings object if the guild is configured.
+        /// Executes <c>notThere</c> otherwise.
+        /// The resulting GuildSettings will be automatically persisted.
+        /// </summary>
+        /// <param name="ctx">This object</param>
+        /// <param name="there">The function to call if the guild is configured</param>
+        /// <param name="notThere">The function to call if the guild is not configured</param>
+        /// <returns>Asynchronous task resolving to the CommandContext, if you wish to reuse it.</returns>
+        public static async Task<CommandContext> IfGuildSettings(this CommandContext ctx, Func<Task> notThere, Func<GuildSettings, Task> there)
+        {
+            var cfg = GetGuildSettings(ctx);
+            if (cfg == null)
+            {
+                await notThere();
+            }
+            else
+            {
+                await there(cfg);
+                await ctx.SetGuildSettingsAsync(cfg);
+            }
+            return ctx;
+        }
+
+        /// <summary>
+        /// Executes a synchronous function <c>there</c> on the GuildSettings object if the guild is configured.
+        /// Executes <c>notThere</c> otherwise.
+        /// The resulting GuildSettings will be automatically persisted.
+        /// </summary>
+        /// <param name="ctx">This object</param>
+        /// <param name="there">The function to call if the guild is configured</param>
+        /// <param name="notThere">The function to call if the guild is not configured</param>
+        /// <returns>Asynchronous task resolving to the CommandContext, if you wish to reuse it.</returns>
+        public static async Task<CommandContext> IfGuildSettings(this CommandContext ctx, Action notThere, Action<GuildSettings> there)
+        {
+            var cfg = GetGuildSettings(ctx);
+            if (cfg == null)
+            {
+                notThere();
+            }
+            else
+            {
+                there(cfg);
+                await ctx.SetGuildSettingsAsync(cfg);
+            }
+            return ctx;
+        }
+
         public static GuildSettings GetGuildSettings(this CommandContext ctx)
         {
             var dbb = ctx.Dependencies.GetDependency<DatabaseContextBuilder>();
