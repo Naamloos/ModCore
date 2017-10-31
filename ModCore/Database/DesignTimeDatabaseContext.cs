31930db117fef7280610684770accd7db55b4cc7
uwx <cadrekucra@gmx.com>
2017-10-31 02:16:17 -0200

    added SQLite support. i'm getting the hang of this

---
+++ ModCore/Database/DesignTimeDatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using ModCore.Entities;
+
+namespace ModCore.Database
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// For SQLite migration creation. To use:
+    /// <code>
+    /// dotnet ef migrations add MyModCoreMigration
+    /// dotnet ef database update
+    /// </code>
+    /// Then simply copy the example.db file to your configured path.
+    /// To update, just copy it back and do the same thing.
+    /// </summary>
+    public class DesignTimeDatabaseContext : IDesignTimeDbContextFactory<DatabaseContext>
+    {
+        public DatabaseContext CreateDbContext(string[] args)
+        {
+            return new DatabaseContext(DatabaseProvider.Sqlite, "Data Source=example.db");
+        }
+    }
+}
