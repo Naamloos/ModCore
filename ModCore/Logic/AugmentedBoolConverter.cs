5371a3dac03f8ea8d75efc5ebf1d366fbdadbf82
Mateusz Brawański <matiz100@windowslive.com>
2017-11-23 08:05:10 +0100

    Moved the augmented bool converter downstream.

---
+++ ModCore/Logic/AugmentedBoolConverter.cs
@@ -0,0 +1,44 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Converters;
+
+namespace ModCore.Logic
+{
+    public class AugmentedBoolConverter : IArgumentConverter<bool>
+    {
+        public bool TryConvert(string value, CommandContext ctx, out bool result)
+        {
+            switch (value.ToLower())
+            {
+                case "y":
+                case "ye":
+                case "ya":
+                case "yup":
+                case "yee":
+                case "davaj":
+                case "yes":
+                case "1":
+                case "on":
+                case "enable":
+                case "да":
+                    result = true;
+                    return true;
+
+                case "n":
+                case "nah":
+                case "nope":
+                case "nyet":
+                case "nada":
+                case "no":
+                case "0":
+                case "off":
+                case "disable":
+                case "нет":
+                    result = false;
+                    return true;
+
+                default:
+                    return bool.TryParse(value, out result);
+            }
+        }
+    }
+}
