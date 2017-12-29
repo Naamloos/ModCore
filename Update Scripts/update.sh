6cbc271a3997271a6eea7d62696e20cafda1d44a
uwx <chris331hansen@outlook.com>
2017-12-29 02:29:16 -0200

    better update

--- Update Scripts/update.sh
+++ Update Scripts/update.sh
@@ -7,7 +7,7 @@ echo "Beginning update package download"
 curl -LO "https://ci.appveyor.com/api/projects/NaamloosDT/modcore/artifacts/ModCore.zip" -o update.zip
 
 echo "Beginning archive extraction"
-unzip -o ModCore%20Release%20Build.zip
+unzip -o update.zip
 
 echo "Restarting process"
 dotnet ModCore.dll $2 $3
