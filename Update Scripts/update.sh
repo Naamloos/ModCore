ca3ff0c2363f34e086e71415d717ac69328411df
uwx <chris331hansen@outlook.com>
2017-12-29 02:34:03 -0200

    FINE

--- Update Scripts/update.sh
+++ Update Scripts/update.sh
@@ -4,10 +4,10 @@ echo "Waiting for process to terminate"
 wait "$1"
 
 echo "Beginning update package download"
-curl -LO "https://ci.appveyor.com/api/projects/NaamloosDT/modcore/artifacts/ModCore.zip" -o update.zip
+curl -LO "https://ci.appveyor.com/api/projects/NaamloosDT/modcore/artifacts/ModCore.zip"
 
 echo "Beginning archive extraction"
-unzip -o update.zip
+unzip -o ModCore.zip
 
 echo "Restarting process"
 dotnet ModCore.dll $2 $3
