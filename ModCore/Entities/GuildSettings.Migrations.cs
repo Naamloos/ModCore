99c3a2d61490e0686d56ff5d96867861c1cbbf53
uwx <cadrekucra@gmx.com>
2018-07-26 02:06:06 -0300

    add DisabledCommands migration

--- ModCore/Entities/GuildSettings.Migrations.cs
+++ ModCore/Entities/GuildSettings.Migrations.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace ModCore.Entities
@@ -15,6 +16,29 @@ namespace ModCore.Entities
         {
             // no getter!
             set => Linkfilter = value;
+        }
+
+        /// <summary>
+        /// Allows deserializing obsolete string-based disabled commands, but not serialization.
+        /// </summary>
+        [JsonProperty("disabledcommands")]
+        [Obsolete("Use " + nameof(DisabledCommands) + " instead!")]
+        public HashSet<string> LegacyDisabledCommands
+        {
+            set
+            {
+                if (DisabledCommands == null)
+                {
+                    DisabledCommands = new HashSet<short>();
+                }
+                using (var db = Program.ModCore.CreateGlobalContext())
+                {
+                    foreach (var s in value)
+                    {
+                        DisabledCommands.Add(db.CommandIds.FirstOrDefault(e => e.Command == s).Id);
+                    }
+                }
+            }
         }
     }
 }
