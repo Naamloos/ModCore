63ed696629129325bb5570e9d57f9a3e243b0dad
uwx <cadrekucra@gmx.com>
2018-07-26 04:44:45 -0300

    fix and document CheckDisable

--- ModCore/Logic/CheckDisableAttribute.cs
+++ ModCore/Logic/CheckDisableAttribute.cs
@@ -18,7 +18,9 @@ namespace ModCore.Logic
 	{
 		public override async Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
 		{
+#if !HSN_DEBUG_ALWAYS_COMMAND_CHECK
 			if (ctx.Member.IsOwner || ctx.User.Id == ctx.Client.CurrentApplication.Owner.Id) return true;
+#endif
 			
 			// don't use GetGuildSettings here
 			// TODO i wrote this a long time ago, and forgot why not to do that.
@@ -26,17 +28,21 @@ namespace ModCore.Logic
 			{
 				var stg = db.GuildConfig.SingleOrDefault(xc => (ulong) xc.GuildId == ctx.Guild.Id)?.GetSettings();
 				if ((stg?.DisabledCommands?.Count ?? 0) == 0) return true;
-			
-				foreach (var commandPart in CommandParts(ctx.Command))
+
+				// the parts to a command eg {"config", "linkfilter", "modules", "all", "on"}
+				var parts = CommandParts(ctx.Command).ToArray();
+				// we take the parts and build a tree, eg
+				// config
+				// config linkfilter
+				// config linkfilter modules
+				// config linkfilter modules all
+				// config linkfilter modules all on
+				var partsStructure = parts.Select((_, i) => string.Join(' ', parts.Take(i+1)));
+				// we check each item of the tree and if any command with that id is in the blacklist, we fail
+				foreach (var commandPart in partsStructure)
 				{
-					var cmd = db.CommandIds.Find(commandPart);
+					var cmd = await db.CommandIds.FindAsync(commandPart);
 					if (cmd == null || !stg.DisabledCommands.Contains(cmd.Id)) continue;
-				}
-				
-				foreach (var disabledCommand in stg.DisabledCommands)
-				{
-					var cmd = db.CommandIds.Find(disabledCommand);
-					if (cmd == null || !ctx.Command.QualifiedName.StartsWith(cmd.Command)) continue;
 					
 					await ctx.RespondAsync("Sorry! that command has been disabled in this guild.");
 					return false;
