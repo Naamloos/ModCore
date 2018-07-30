05de64bebdbb918dc8d00ed2852a066853f15f8e
uwx <cadrekucra@gmx.com>
2018-07-29 23:07:02 -0300

    finish porting DatabaseInfo

--- ModCore/Database/DatabaseInfo.cs
+++ ModCore/Database/DatabaseInfo.cs
@@ -1,12 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ModCore.Logic.EntityFramework.AttributeImpl;
 
 namespace ModCore.Database
 {
     [Table("mcore_database_info")]
     public class DatabaseInfo
     {
+        [Column("id")]
         public int Id { get; set; }
+        
+        [Index("mcore_database_info_meta_key_key", IsUnique = true)]
+        [Column("meta_key")]
+        [Required]
         public string MetaKey { get; set; }
+        
+        [Column("meta_value")]
+        [Required]
         public string MetaValue { get; set; }
     }
 }
