20ed7d822fae7041b4986aec87b837be478ab41e
uwx <cadrekucra@gmx.com>
2018-07-31 21:23:15 -0300

    remove dem notes

--- ModCore/Database/DatabaseContext.cs
+++ ModCore/Database/DatabaseContext.cs
@@ -59,26 +59,11 @@ namespace ModCore.Database
 
         protected override void OnModelCreating(ModelBuilder model)
         {
-            model.BuildCustomAttributes(this);
-            
-            // TODO: at the end of moving all of these to annotations
-            // > add a new EF migration, don't apply it, generate a script for it, theoretically there should be nothing
-            //   changed, otherwise we messed up
-            // > test the bot (obviously)
-            
-            // TODO https://stackoverflow.com/questions/2878272/when-should-i-use-primary-key-or-index
-            // the use of indices here makes sense, however, should we add [Key] to the .Id properties?
-            
-            // TODO change index_id in DatabaseCommandId to somethign less likely to conflict, like
-            // mcore_cmd_state_index_id
-            
             // https://www.learnentityframeworkcore.com/configuration/data-annotation-attributes
             // this site is GOLD for finding out the data annotation equivalents to fluent API methods, and finding out 
             // whether or not they even exist
             
-            // TODO is cmd state index_id even necessary? isn't it duplicate, since we already have a key?
-            // is the alternate key needed? i guess making id an alternate key makes it unique, which is
-            // important.
+            model.BuildCustomAttributes(this);
         }
     }
 }
