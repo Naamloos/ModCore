a2c534d26765c5fdb94935e6ef5e1293213d4377
NaamloosDT <NaamloosDT@gmail.com>
2018-08-18 00:24:03 +0200

    fix

--- ModCore/Listeners/ErrorLog.cs
+++ ModCore/Listeners/ErrorLog.cs
@@ -18,139 +18,158 @@ using ModCore.Logic.Extensions;
 
 namespace ModCore.Listeners
 {
-    public class ErrorLog
-    {
-        [AsyncListener(EventTypes.CommandErrored)]
-        public static async Task CommandError(ModCoreShard bot, CommandErrorEventArgs e)
-        {
-            var cfg = e.Context.GetGuildSettings() ?? new GuildSettings();
-            var ctx = e.Context;
+	public class ErrorLog
+	{
+		[AsyncListener(EventTypes.CommandErrored)]
+		public static async Task CommandError(ModCoreShard bot, CommandErrorEventArgs e)
+		{
+			var cfg = e.Context.GetGuildSettings() ?? new GuildSettings();
+			var ctx = e.Context;
 
-            if (await NotifyCommandDisabled(e, cfg, ctx)) return;
-            
-            if (await NotifyCommandNotFound(bot, e, cfg, ctx)) return;
+			if (await NotifyCommandDisabled(e, cfg, ctx)) return;
 
-            await NotifyCommandErrored(e, cfg, ctx);
-        }
+			if (await NotifyCommandNotFound(bot, e, cfg, ctx)) return;
 
-        private static async Task NotifyCommandErrored(CommandErrorEventArgs e, GuildSettings cfg, CommandContext ctx)
-        {
-            var qualifiedName = e.Command.QualifiedName;
-            switch (cfg.CommandError.Chat)
-            {
-                default:
-                case CommandErrorVerbosity.None:
-                    #if DEBUG
-                    Console.WriteLine($"Oopsie woopsie! {e.Exception}");
-                    #endif
-                    break;
+			await NotifyCommandErrored(e, cfg, ctx);
+		}
 
-                case CommandErrorVerbosity.Name:
-                    await ctx.SafeRespondAsync($"**Command {qualifiedName} Errored!**");
-                    #if DEBUG
-                    Console.WriteLine($"Oopsie woopsie! {e.Exception}");
-                    #endif
-                    break;
-                case CommandErrorVerbosity.NameDesc:
-                    await DescribeCommandErrorAsync(e.Exception, qualifiedName, ctx);
-                    break;
-                case CommandErrorVerbosity.Exception:
-                    var stream = new MemoryStream();
-                    var writer = new StreamWriter(stream);
-                    writer.Write(e.Exception.StackTrace);
-                    writer.Flush();
-                    stream.Position = 0;
-                    await ctx.RespondWithFileAsync("exception.txt", stream,
-                        $"**Command `{qualifiedName}` Errored!**\n`{e.Exception.GetType()}`:\n{e.Exception.Message}");
-                    break;
-            }
+		private static async Task NotifyCommandErrored(CommandErrorEventArgs e, GuildSettings cfg, CommandContext ctx)
+		{
+			var qualifiedName = e.Command.QualifiedName;
+			switch (cfg.CommandError.Chat)
+			{
+				default:
+				case CommandErrorVerbosity.None:
+#if DEBUG
+					Console.WriteLine($"Oopsie woopsie! {e.Exception}");
+#endif
+					break;
 
-            if (cfg.ActionLog.Enable)
-            {
-                switch (cfg.CommandError.ActionLog)
-                {
-                    default:
-                    case CommandErrorVerbosity.None:
-                        break;
+				case CommandErrorVerbosity.Name:
+					await ctx.SafeRespondAsync($"**Command {qualifiedName} Errored!**");
+#if DEBUG
+					Console.WriteLine($"Oopsie woopsie! {e.Exception}");
+#endif
+					break;
+				case CommandErrorVerbosity.NameDesc:
+					await DescribeCommandErrorAsync(e.Exception, qualifiedName, ctx);
+					break;
+				case CommandErrorVerbosity.Exception:
+					var stream = new MemoryStream();
+					var writer = new StreamWriter(stream);
+					writer.Write(e.Exception.StackTrace);
+					writer.Flush();
+					stream.Position = 0;
+					await ctx.RespondWithFileAsync("exception.txt", stream,
+						$"**Command `{qualifiedName}` Errored!**\n`{e.Exception.GetType()}`:\n{e.Exception.Message}");
+					break;
+			}
 
-                    case CommandErrorVerbosity.Name:
-                        await ctx.LogMessageAsync($"**Command {qualifiedName} errored!**\n`{e.Exception.GetType()}`");
-                        break;
-                    case CommandErrorVerbosity.NameDesc:
-                        await ctx.LogMessageAsync(
-                            $"**Command {qualifiedName} errored!**\n`{e.Exception.GetType()}`:\n{e.Exception.Message}");
-                        break;
-                    case CommandErrorVerbosity.Exception:
-                        var st = e.Exception.StackTrace;
+			if (cfg.ActionLog.Enable)
+			{
+				switch (cfg.CommandError.ActionLog)
+				{
+					default:
+					case CommandErrorVerbosity.None:
+						break;
 
-                        st = st.Length > 1000 ? st.Substring(0, 1000) : st;
-                        var b = new DiscordEmbedBuilder().WithDescription(st);
-                        await ctx.LogMessageAsync(
-                            $"**Command {qualifiedName} {e.Command.Overloads.First().Arguments} errored!**\n`{e.Exception.GetType()}`:\n{e.Exception.Message}",
-                            b);
-                        break;
-                }
-            }
-        }
+					case CommandErrorVerbosity.Name:
+						await ctx.LogMessageAsync($"**Command {qualifiedName} errored!**\n`{e.Exception.GetType()}`");
+						break;
+					case CommandErrorVerbosity.NameDesc:
+						await ctx.LogMessageAsync(
+							$"**Command {qualifiedName} errored!**\n`{e.Exception.GetType()}`:\n{e.Exception.Message}");
+						break;
+					case CommandErrorVerbosity.Exception:
+						var st = e.Exception.StackTrace;
 
-        private static async Task<bool> NotifyCommandNotFound(ModCoreShard bot, CommandErrorEventArgs e, 
-            GuildSettings cfg, CommandContext ctx)
-        {
-            if (!(e.Exception is CommandNotFoundException commandNotFound)) return false;
-            
-            // return instead of proceeding, since qualifiedName below this will throw
-            // (since there is no Command obj) 
-            if (!cfg.SpellingHelperEnabled) return true;
+						st = st.Length > 1000 ? st.Substring(0, 1000) : st;
+						var b = new DiscordEmbedBuilder().WithDescription(st);
+						await ctx.LogMessageAsync(
+							$"**Command {qualifiedName} {e.Command.Overloads.First().Arguments} errored!**\n`{e.Exception.GetType()}`:\n{e.Exception.Message}",
+							b);
+						break;
+				}
+			}
+		}
 
-            // TODO: this only gives the first invalid token, so "a b" will just recognize as "a".
-            //       it should probably take into consideration all the tokens.
-            var attemptedName = commandNotFound.CommandName;
-            try
-            {
-                // TODO: i intended on using the library for more than just this,
-                //       but i ended up using it like this, so can probably just copy code from that lib 
-                //       instead of nugetting it
-                var leveshtein = new Levenshtein(); // lower is better
+		private static async Task<bool> NotifyCommandNotFound(ModCoreShard bot, CommandErrorEventArgs e,
+			GuildSettings cfg, CommandContext ctx)
+		{
+			if (!(e.Exception is CommandNotFoundException commandNotFound)) return false;
 
-                var ordered = bot.SharedData.Commands
-                    .Where(c => (!(c.cmd is CommandGroup group) || @group.IsExecutableWithoutSubcommands) && (c.cmd.RunChecksAsync(ctx,true))
-                    .Select(c => (qualifiedName: c.cmd.QualifiedName, description: c.cmd.Description))
-                    .OrderBy(c => leveshtein.Distance(attemptedName, c.qualifiedName))
-                    .DistinctBy(c => c.qualifiedName).Take(1).ToArray();
+			// return instead of proceeding, since qualifiedName below this will throw
+			// (since there is no Command obj) 
+			if (!cfg.SpellingHelperEnabled) return true;
 
-                await new ModernEmbedBuilder
-                {
-                    Title = "Command **" + attemptedName.Truncate(200) + "** not found",
-                    Description = "Did you mean...",
-                    Fields = ordered.Select(c =>
-                        new DuckField(c.qualifiedName.Truncate(256), c.description?.Truncate(999) ?? "<no desc>")).ToList()
-                }.Send(ctx.Channel);
-            }
-            catch (Exception ex)
-            {
-                e.Context.Client.DebugLogger.LogMessage(LogLevel.Critical, "CommandErrored", ex.ToString(),
-                    DateTime.Now);
-            }
+			// TODO: this only gives the first invalid token, so "a b" will just recognize as "a".
+			//       it should probably take into consideration all the tokens.
+			var attemptedName = commandNotFound.CommandName;
+			try
+			{
+				// TODO: i intended on using the library for more than just this,
+				//       but i ended up using it like this, so can probably just copy code from that lib 
+				//       instead of nugetting it
+				var leveshtein = new Levenshtein(); // lower is better
 
-            return true;
+				// TODO: add checks
+				var preorder = bot.SharedData.Commands.Select(x => x.cmd).ToList();
+				foreach (var c in preorder)
+				{
+					if (!await CanExecute(c, ctx))
+						preorder.Remove(c);
+				}
 
-        }
+				var ordered = preorder
+					.Where(c => !(c is CommandGroup group) || @group.IsExecutableWithoutSubcommands)
+					.Select(c => (qualifiedName: c.QualifiedName, description: c.Description))
+					.OrderBy(c => leveshtein.Distance(attemptedName, c.qualifiedName))
+					.DistinctBy(c => c.qualifiedName).Take(1).ToArray();
 
-        private static async Task<bool> NotifyCommandDisabled(CommandErrorEventArgs e, GuildSettings cfg,
-            CommandContext ctx)
-        {
-            if (!(e.Exception is ChecksFailedException checksFailed) ||
-                !checksFailed.FailedChecks.Any(x => x is CheckDisableAttribute)) return false;
+				await new ModernEmbedBuilder
+				{
+					Title = "Command **" + attemptedName.Truncate(200) + "** not found",
+					Description = "Did you mean...",
+					Fields = ordered.Select(c =>
+						new DuckField(c.qualifiedName.Truncate(256), c.description?.Truncate(999) ?? "<no desc>")).ToList()
+				}.Send(ctx.Channel);
+			}
+			catch (Exception ex)
+			{
+				e.Context.Client.DebugLogger.LogMessage(LogLevel.Critical, "CommandErrored", ex.ToString(),
+					DateTime.Now);
+			}
 
-            if (cfg.NotifyDisabledCommand)
-            {
-                await ctx.ElevatedRespondAsync("Sorry! that command has been disabled in this guild.");
-            }
-            
-            return true;
-        }
+			return true;
 
-        public static async Task DescribeCommandErrorAsync(Exception ex, string cmd, CommandContext ctx)
+		}
+
+		private static async Task<bool> CanExecute(Command cmd, CommandContext ctx)
+		{
+			if (cmd.Parent != null)
+				return await this.CanExecute(cmd.Parent, ctx).ConfigureAwait(false);
+
+			var fchecks = await cmd.RunChecksAsync(ctx, false).ConfigureAwait(false);
+			if (fchecks.Any())
+				return false;
+			return true;
+		}
+
+		private static async Task<bool> NotifyCommandDisabled(CommandErrorEventArgs e, GuildSettings cfg,
+			CommandContext ctx)
+		{
+			if (!(e.Exception is ChecksFailedException checksFailed) ||
+				!checksFailed.FailedChecks.Any(x => x is CheckDisableAttribute)) return false;
+
+			if (cfg.NotifyDisabledCommand)
+			{
+				await ctx.ElevatedRespondAsync("Sorry! that command has been disabled in this guild.");
+			}
+
+			return true;
+		}
+
+		public static async Task DescribeCommandErrorAsync(Exception ex, string cmd, CommandContext ctx)
 		{
 			if (ex is ChecksFailedException checksFailed)
 			{
@@ -181,5 +200,5 @@ namespace ModCore.Listeners
 			}
 			await ctx.SafeRespondAsync($"**Command {cmd} Errored!**\n{ex.Message}");
 		}
-    }
+	}
 }
