5294af6517137319a78b411c6f48495b6ec7cf2f
Ryan <naamloosdt@gmail.com>
2020-08-01 23:23:27 +0200

    Remove obsolete auto-update scripts

--- Update Scripts/download.ps1
+++
@@ -1,2 +0,0 @@
-$client = new-object System.Net.WebClient
-$client.DownloadFile("https://ci.appveyor.com/api/projects/NaamloosDT/modcore/artifacts/ModCore.zip","update.zip")
--- Update Scripts/update.bat
+++
@@ -1,4 +0,0 @@
-Wait 5
-Powershell.exe -executionpolicy remotesigned -File download.ps1
-"C:\Program Files\7-Zip\7z.exe" e update.zip -ao
-start dotnet ModCore.dll %1 %2
