2f823920cefefa2a085aab386ad0995d60b66f50
Ryan de Jonge <naamloosdt@gmail.com>
2021-05-01 00:29:18 +0200

    Add ConfigNext command class to eventually replace Config

---
+++ ModCore/Commands/ConfigNext.cs
@@ -0,0 +1,24 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModCore.Commands
+{
+    [Group("confignext")]
+    [Aliases("cfgn")]
+    [Description("Guild configuration options.")]
+    [RequireUserPermissions(Permissions.ManageGuild)]
+    public class ConfigNext : BaseCommandModule
+    {
+        [GroupCommand]
+        [Description("Runs guild config.")]
+        public async Task ExecuteGroupAsync(CommandContext ctx)
+        {
+
+        }
+    }
+}
