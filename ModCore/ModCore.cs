4bbeff74cc5be2fd5afd3f3515deb87c9687501d
Ryan de Jonge <naamloosdt@gmail.com>
2022-05-17 21:57:56 +0200

    some spacing fixes in ModCore.cs

--- ModCore/ModCore.cs
+++ ModCore/ModCore.cs
@@ -27,11 +27,11 @@ namespace ModCore
 {
     public class ModCore
     {
-		public Settings Settings { get; private set; }
-		public SharedData SharedData { get; private set; }
-		public List<ModCoreShard> Shards { get; set; }
-	    
-	    private DatabaseContextBuilder GlobalContextBuilder { get; set; }
+        public Settings Settings { get; private set; }
+        public SharedData SharedData { get; private set; }
+        public List<ModCoreShard> Shards { get; set; }
+
+        private DatabaseContextBuilder GlobalContextBuilder { get; set; }
         private CancellationTokenSource CTS { get; set; }
         private Perspective PerspectiveApi { get; set; }
 
@@ -48,50 +48,50 @@ namespace ModCore
 
             var input = File.ReadAllText("settings.json", new UTF8Encoding(false));
             Settings = JsonConvert.DeserializeObject<Settings>(input);
-			GlobalContextBuilder = Settings.Database.CreateContextBuilder();
+            GlobalContextBuilder = Settings.Database.CreateContextBuilder();
 
-			if (args.Contains("--migrate"))
+            if (args.Contains("--migrate"))
             {
-				this.migrate();
-				return;
+                this.migrate();
+                return;
             }
 
-			// Saving config with same values but updated fields
-			var newjson = JsonConvert.SerializeObject(Settings, Formatting.Indented);
-			File.WriteAllText("settings.json", newjson, new UTF8Encoding(false));
+            // Saving config with same values but updated fields
+            var newjson = JsonConvert.SerializeObject(Settings, Formatting.Indented);
+            File.WriteAllText("settings.json", newjson, new UTF8Encoding(false));
 
             PerspectiveApi = new Perspective(Settings.PerspectiveToken);
 
             Shards = new List<ModCoreShard>();
             InitializeSharedData(args);
 
-	        // cnext data that is consistent across shards, so it's fine to share it
-	        for (var i = 0; i < Settings.ShardCount; i++)
+            // cnext data that is consistent across shards, so it's fine to share it
+            for (var i = 0; i < Settings.ShardCount; i++)
             {
                 var shard = new ModCoreShard(Settings, i, SharedData);
                 shard.Initialize();
                 Shards.Add(shard);
-	            if (i == 0)
-	            {
-		            SharedData.Initialize(shard);
-	            }
+                if (i == 0)
+                {
+                    SharedData.Initialize(shard);
+                }
             }
 
-	        await InitializeDatabaseAsync();
+            await InitializeDatabaseAsync();
 
-	        foreach (var shard in Shards)
-		        await shard.RunAsync();
+            foreach (var shard in Shards)
+                await shard.RunAsync();
 
-	        await BuildWebHost().RunAsync(CTS.Token);
+            await BuildWebHost().RunAsync(CTS.Token);
 
-			await WaitForCancellation();
+            await WaitForCancellation();
 
-			foreach (var shard in Shards)
-				await shard.DisconnectAndDispose();
+            foreach (var shard in Shards)
+                await shard.DisconnectAndDispose();
 
-			this.SharedData.CancellationTokenSource.Dispose();
-			this.SharedData.TimerData.Cancel.Cancel();
-			this.SharedData.TimerSempahore.Dispose();
+            this.SharedData.CancellationTokenSource.Dispose();
+            this.SharedData.TimerData.Cancel.Cancel();
+            this.SharedData.TimerSempahore.Dispose();
         }
 
         /// <summary>
@@ -100,58 +100,59 @@ namespace ModCore
         private void InitializeSharedData(string[] args)
         {
             CTS = new CancellationTokenSource();
-	        SharedData = new SharedData
-	        {
-		        CancellationTokenSource = CTS,
-		        ProcessStartTime = Process.GetCurrentProcess().StartTime,
-		        Perspective = PerspectiveApi,
-		        BotManagers = Settings.BotManagers,
-		        DefaultPrefix = Settings.DefaultPrefix,
-		        ModCore = this
-	        };
+            SharedData = new SharedData
+            {
+                CancellationTokenSource = CTS,
+                ProcessStartTime = Process.GetCurrentProcess().StartTime,
+                Perspective = PerspectiveApi,
+                BotManagers = Settings.BotManagers,
+                DefaultPrefix = Settings.DefaultPrefix,
+                ModCore = this
+            };
         }
 
-	    private async Task InitializeDatabaseAsync()
-	    {
-			// Fix for legacy timestamp behavior. This might need a "proper" fix in the future
-			AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-			// add command id mappings if they don't already exist
-			var modifications = new List<string>();
-		    using (var db = this.CreateGlobalContext())
-		    {
-			    foreach (var (name, _) in SharedData.Commands)
-			    {
-				    if (db.CommandIds.FirstOrDefault(e => e.Command == name) != null) continue;
-				    Console.WriteLine($"Registering new command in db: {name}");
-				    
-				    modifications.Add(name);
-				    await db.CommandIds.AddAsync(new DatabaseCommandId()
-				    {
-					    Command = name
-				    });
-			    }
-			    await db.SaveChangesAsync();
-		    }
-            
-	    }
+        private async Task InitializeDatabaseAsync()
+        {
+            // Fix for legacy timestamp behavior. This might need a "proper" fix in the future
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            // add command id mappings if they don't already exist
+            var modifications = new List<string>();
+            using (var db = this.CreateGlobalContext())
+            {
+                foreach (var (name, _) in SharedData.Commands)
+                {
+                    if (db.CommandIds.FirstOrDefault(e => e.Command == name) != null) continue;
+                    Console.WriteLine($"Registering new command in db: {name}");
+
+                    modifications.Add(name);
+                    await db.CommandIds.AddAsync(new DatabaseCommandId()
+                    {
+                        Command = name
+                    });
+                }
+                await db.SaveChangesAsync();
+            }
+
+        }
         public async Task WaitForCancellation()
         {
             while (!CTS.IsCancellationRequested)
                 await Task.Delay(500);
         }
 
-		private IHost BuildWebHost()
-		{
-			var container = new CoreContainer
-			{
-				mcore = this
-			};
+        private IHost BuildWebHost()
+        {
+            var container = new CoreContainer
+            {
+                mcore = this
+            };
 
-			var mservice = new ServiceDescriptor(container.GetType(), container);
+            var mservice = new ServiceDescriptor(container.GetType(), container);
 
-			return new HostBuilder()
-				.UseContentRoot(Directory.GetCurrentDirectory())
-				.ConfigureWebHostDefaults(webBuilder => {
+            return new HostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
                     webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
                      {
                          config.AddJsonFile(
@@ -159,58 +160,58 @@ namespace ModCore
                      });
 
                     webBuilder.UseStartup<Startup>()
-						.ConfigureServices(x => x.Add(mservice))
-						.UseUrls("http://0.0.0.0:6969");
-				})
-				.Build();
-		}
-	    
-	    public DatabaseContext CreateGlobalContext()
-	    {
-		    return GlobalContextBuilder.CreateContext();
-	    }
+                        .ConfigureServices(x => x.Add(mservice))
+                        .UseUrls("http://0.0.0.0:6969");
+                })
+                .Build();
+        }
 
-		private void migrate()
+        public DatabaseContext CreateGlobalContext()
         {
-			using var db = this.CreateGlobalContext();
-			var pending = db.Database.GetPendingMigrations();
+            return GlobalContextBuilder.CreateContext();
+        }
 
-			if(pending.Count() < 1)
+        private void migrate()
+        {
+            using var db = this.CreateGlobalContext();
+            var pending = db.Database.GetPendingMigrations();
+
+            if (pending.Count() < 1)
             {
-				Console.WriteLine("No migrations pending.");
-				return;
+                Console.WriteLine("No migrations pending.");
+                return;
             }
 
-			Console.WriteLine("Pending migrations:");
-			foreach(var migration in pending)
+            Console.WriteLine("Pending migrations:");
+            foreach (var migration in pending)
             {
-				Console.WriteLine(migration);
+                Console.WriteLine(migration);
             }
 
-			Console.WriteLine("Please ensure you have made a backup of your database. Migrate? (Y/n)");
-			var response = Console.ReadKey();
-			if (response.KeyChar == 'y')
-			{
-				Console.WriteLine("Attempting to apply migrations...");
-				try
-				{
-					this.CreateGlobalContext().Database.Migrate();
-				}
-				catch(Exception ex)
+            Console.WriteLine("Please ensure you have made a backup of your database. Migrate? (Y/n)");
+            var response = Console.ReadKey();
+            if (response.KeyChar == 'y')
+            {
+                Console.WriteLine("Attempting to apply migrations...");
+                try
                 {
-					Console.WriteLine("Migrations failed...");
-					Console.WriteLine(ex.Message);
+                    this.CreateGlobalContext().Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Migrations failed...");
+                    Console.WriteLine(ex.Message);
                 }
                 finally
                 {
-					Console.WriteLine("Migrations applied.");
-				}
-			}
-			else
-			{
-				Console.WriteLine("Operation canceled.");
-			}
-			return;
-		}
-	}
+                    Console.WriteLine("Migrations applied.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Operation canceled.");
+            }
+            return;
+        }
+    }
 }
