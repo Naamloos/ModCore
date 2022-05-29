7ef9324a2265ea2dd5434ea9dd0db602590fdab3
Lunar Starstrum <lunar@forsaken-borders.net>
2022-05-28 19:29:07 -0500

    Update MentionUlongConverter.cs
    Code golfing!

--- ModCore/Logic/MentionUlongConverter.cs
+++ ModCore/Logic/MentionUlongConverter.cs
@@ -8,17 +8,14 @@ namespace ModCore.Logic
 {
     public class MentionUlongConverter : IArgumentConverter<ulong>
     {
-        public static readonly Regex mentionRegex = new Regex(@"^<(#|@)!?([0-9]+)>$");
-        public async Task<Optional<ulong>> ConvertAsync(string value, CommandContext ctx)
+        public static readonly Regex MentionRegex = new(@"^<(#|@)!?([0-9]+)>$", RegexOptions.Compiled);
+        public Task<Optional<ulong>> ConvertAsync(string value, CommandContext ctx)
         {
             if (ulong.TryParse(value, out var result))
-                return new Optional<ulong>(result);
+                return Task.FromResult(new Optional<ulong>(result));
 
-            var match = mentionRegex.Match(value);
-            if (match.Success && ulong.TryParse(match.Groups[2].Value, out var mention))
-                return new Optional<ulong>(mention);
-
-            return default;
+            Match match = MentionRegex.Match(value);
+            return Task.FromResult(match.Success && ulong.TryParse(match.Groups[2].Value, out var mention) ? new Optional<ulong>(mention) : default);
         }
     }
 }
