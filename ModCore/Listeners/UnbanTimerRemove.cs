57e1ca40c489018fbd6d0fa8f7c0962119751a1e
Ryan de Jonge <ryandejonge@outlook.com>
2023-05-24 21:38:35 +0200

    Fix build error

--- ModCore/Listeners/UnbanTimerRemove.cs
+++ ModCore/Listeners/UnbanTimerRemove.cs
@@ -13,11 +13,11 @@ namespace ModCore.Listeners
     public class UnbanTimerRemove
     {
         [AsyncListener(EventType.GuildBanRemoved)]
-        public static async Task CommandError(GuildBanRemoveEventArgs eventargs, DatabaseContextBuilder database, DiscordClient client, SharedData sharedData)
+        public static async Task GuildBanRemoved(GuildBanRemoveEventArgs eventargs, DatabaseContextBuilder database, DiscordClient client, SharedData sharedData)
         {
             var timer = Timers.FindNearestTimer(TimerActionType.Unban, eventargs.Member.Id, 0, eventargs.Guild.Id, database);
             if (timer != null)
-                await Timers.UnscheduleTimerAsync(timer, client, database, sharedData);
+                await Timers.UnscheduleTimersAsync(timer);
         }
     }
 }
