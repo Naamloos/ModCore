376cf06f0122d1acb161b02a5a1248d9afb6b467
Ryan de Jonge <ryandejonge@outlook.com>
2024-01-07 01:49:52 +0100

    Small fix when reconnecting, to not re-register commands.

--- src/ModCore.Services.Shard/EventHandlers/StartupEvents.cs
+++ src/ModCore.Services.Shard/EventHandlers/StartupEvents.cs
@@ -22,6 +22,8 @@ namespace ModCore.Services.Shard.EventHandlers
         private readonly DiscordRest _rest;
         private readonly InteractionService _interactions;
 
+        private bool commandsRegistered = false;
+
         public StartupEvents(ILogger<StartupEvents> logger, DiscordRest rest, InteractionService interactions)
         {
             _logger = logger;
@@ -39,15 +41,19 @@ namespace ModCore.Services.Shard.EventHandlers
             _logger.LogInformation("Ready from event handler! User is {0} with ID {1}.",
                 data.User.Username, data.User.Id);
             var application = await _rest.GetApplicationAsync(data.Application.Id);
-            _interactions.RegisterCommands(Assembly.GetExecutingAssembly());
 
             if (application.Success)
             {
                 _logger.LogInformation("Application is registered under ID {0}. Owner username is {1}.", data.Application.Id, application.Value!.Owner!.Value.Username);
-                if (data.Shard.HasValue && data.Shard.Value[0] == 0)
+                if (!commandsRegistered)
                 {
-                    // Only send commands if we know we're definitely on shard 0!!
-                    await _interactions.PublishCommands(application.Value.Id);
+                    _interactions.RegisterCommands(Assembly.GetExecutingAssembly());
+                    if (data.Shard.HasValue && data.Shard.Value[0] == 0)
+                    {
+                        // Only send commands if we know we're definitely on shard 0!!
+                        await _interactions.PublishCommands(application.Value.Id);
+                    }
+                    commandsRegistered = true;
                 }
             }
             else
